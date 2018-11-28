3d0c0f988a9d83ceec0cbbc9c71d7b04cd4eaafa
Stone Gu <stone_gu@hotmail.com>
2018-11-27 23:42:23 -0500

    binary tree

--- algorithms/src/test/java/binarySearchTree/BinaryTreeTest.java
+++ algorithms/src/test/java/binarySearchTree/BinaryTreeTest.java
@@ -1,13 +1,18 @@
 package binarySearchTree;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinaryTreeTest {
-
-    @Test
-    public void testCreateTree() {
+    @DataProvider(name = "mockTree")
+    public Object[][] getMockTree() {
+    /*
+                   6
+               4      8
+            3   5   7   9
+    */
         BinaryTree binaryTree = new BinaryTree();
         binaryTree.add(6);
         binaryTree.add(4);
@@ -17,11 +22,54 @@ public class BinaryTreeTest {
         binaryTree.add(7);
         binaryTree.add(9);
 
+        return new Object[][] {
+                {
+                        binaryTree
+                }
+        };
+    }
+
+    @Test(dataProvider = "mockTree")
+    public void testCreateTree(BinaryTree binaryTree) {
+
+        StringBuilder sb = new StringBuilder();
+        binaryTree.preOrderTraversal(binaryTree.getRoot(), sb);
+
         assertThat(binaryTree).isNotNull();
+        assertThat(sb.toString()).isEqualTo("6435879");
+    }
 
+    @Test(dataProvider = "mockTree")
+    public void testContainsNode(BinaryTree binaryTree) {
+        assertThat(binaryTree.containsNode(6)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(4)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(3)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(5)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(8)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(7)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(9)).isEqualTo(true);
+        assertThat(binaryTree.containsNode(1)).isEqualTo(false);
+    }
 
+    @Test(dataProvider = "mockTree")
+    public void testDeleteNode(BinaryTree binaryTree) {
+        binaryTree.delete(3);
+        StringBuilder sb = new StringBuilder();
+        binaryTree.preOrderTraversal(binaryTree.getRoot(), sb);
+        assertThat(sb.toString()).isEqualTo("645879");
+
+        binaryTree.delete(4);
+        sb = new StringBuilder();
+        binaryTree.preOrderTraversal(binaryTree.getRoot(), sb);
+        assertThat(sb.toString()).isEqualTo("65879");
+
+        binaryTree.delete(6);
+        sb = new StringBuilder();
+        binaryTree.preOrderTraversal(binaryTree.getRoot(), sb);
+        assertThat(sb.toString()).isEqualTo("7589");
 
     }
+
 
 
 }
